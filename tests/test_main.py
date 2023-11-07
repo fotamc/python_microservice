55d4db0c0fba7b8ecf1d0b43242d3c51414bc6f7
Mihai Fota <fota.mihai@yahoo.com>
2023-11-07 20:15:25 +0200

    Initial Commit

---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message":"Hello World"}
+
+def test_read_data():
+    response = client.get("/data/Ion")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World Ion"}
