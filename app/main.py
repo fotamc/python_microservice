1f07af0547592c4f04a86867bb18aea9336bce48
Mihai Fota <fota.mihai@yahoo.com>
2023-11-09 19:51:29 +0200

    fixed test

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
+"""Module docstring"""
 from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def read_root():
+    """funtion docstring"""
     return {"message": "Hello World"}
+
 
 @app.get("/data/{name}")
 async def read_data(name: str):
-    return {"message": f"Hello World {name}"}
+    return {"message": f"Hello World {name.capitalize()}"}
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,11 @@
+"""Module docstring"""
 from fastapi.testclient import TestClient
 from app.main import app
 
 client = TestClient(app)
 
 def test_read_root():
+     """funtion docstring"""
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message":"Hello World"}
@@ -12,3 +14,4 @@ def test_read_data():
     response = client.get("/data/Ion")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World Ion"}
+    
