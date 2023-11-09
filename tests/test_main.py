264f8dc3a4811909dd3ed8cb071d79ed058cbf1e
Mihai Fota <fota.mihai@yahoo.com>
2023-11-09 20:02:41 +0200

    fixed test1

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,16 @@
-"""doc string"""
+"""Module docstring"""
 from fastapi.testclient import TestClient
 from app.main import app
 
 client = TestClient(app)
 
 def test_read_root():
-    """doc string"""
+    """funtion docstring"""
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message":"Hell on World"}
 
 def test_read_data():
-    """doc string"""
     response = client.get("/data/ion")
     assert response.status_code == 200
     assert response.json() == {"message":"Hell on World ion"}
